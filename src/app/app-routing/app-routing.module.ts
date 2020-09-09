a59d3f87deee246f613664064dbb0af057001a64
Luis Eduardo D <luis.diaz.car@gmail.com>
2020-09-08 22:23:05 -0500

    refactor to create children routes

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -6,23 +6,29 @@ import { RegisterComponent } from '../auth/register/register.component'
 import { ProgressComponent } from '../pages/progress/progress.component'
 import { Grafica1Component } from '../pages/grafica1/grafica1.component'
 import { NotpagefoundComponent } from '../pages/notpagefound/notpagefound.component'
+import { MainDashboardComponent } from '../pages/main-dashboard/main-dashboard.component'
 
 const routes: Routes = [
-  { path: 'dashboard', component: DashboardComponent },
+  {
+    path: '',
+    component: MainDashboardComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'progress', component: ProgressComponent },
+      { path: 'grafica1', component: Grafica1Component },
+      { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
+    ]
+  },
+
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'progress', component: ProgressComponent },
-  { path: 'grafica1', component: Grafica1Component },
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: '**', component: NotpagefoundComponent }
 
+  { path: '**', component: NotpagefoundComponent }
 ]
 
 @NgModule({
   declarations: [],
-  imports: [
-    RouterModule.forRoot(routes)
-  ],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
